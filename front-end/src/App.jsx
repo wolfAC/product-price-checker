f2ea4f4054a48d3e51b841c5038dac7ef640aafc
Anbu chezhiyan <anbu8132609@gmail.com>
2024-06-07 00:12:38 +0530

    update the changes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,7 +6,6 @@ const { Search } = Input;
 function App() {
   const [itemsToShow, setItemsToShow] = useState(10);
   const [searchKey, setSearchKey] = useState(null);
-  const [loading, setLoading] = useState(false);
   const [filteredData, setFilteredData] = useState(null);
   const containerRef = useRef(null);
 
@@ -29,27 +28,15 @@ function App() {
       container.addEventListener("scroll", handleScroll);
       return () => container.removeEventListener("scroll", handleScroll);
     }
+
     // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    setLoading(true);
     if (!searchKey) {
       setFilteredData(JSON_DATA);
-    } else if (/^[0-9]+$/.test(searchKey)) {
-      setFilteredData(
-        JSON_DATA.filter((data) => data.price.includes(searchKey))
-      );
-    } else if (/[a-zA-Z]/.test(searchKey)) {
-      setFilteredData(
-        JSON_DATA.filter((data) =>
-          data.productName.toLocaleLowerCase().includes(searchKey)
-        )
-      );
     }
-    setLoading(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchKey, JSON_DATA]);
+  }, [searchKey]);
 
   const onChangeHandler = (event) => {
     setSearchKey(event.target.value);
@@ -57,8 +44,8 @@ function App() {
 
   return (
     <div className="overflow-hidden h-full">
-      <div className="text-[50px] pb-[24px] text-center font-serif">
-        PRODUCT PRICE CHECKER
+      <div className="text-[50px] pb-[24px] text-center edu-vic-wa-nt-beginner-regular">
+        C & R<h1>COMPARE AND RECOMMENDATIONS</h1>
       </div>
       <div className="w-full flex justify-center">
         {
@@ -66,8 +53,26 @@ function App() {
             size="large"
             className="w-[50%]"
             onChange={onChangeHandler}
+            onPressEnter={() => {
+              if (!searchKey) {
+                setFilteredData(JSON_DATA);
+              } else if (/^[0-9]+$/.test(searchKey)) {
+                setFilteredData(
+                  JSON_DATA.filter((data) =>
+                    data.price.includes(searchKey.toLocaleLowerCase())
+                  )
+                );
+              } else if (/[a-zA-Z]/.test(searchKey)) {
+                setFilteredData(
+                  JSON_DATA.filter((data) =>
+                    data.productName
+                      .toLocaleLowerCase()
+                      .includes(searchKey.toLocaleLowerCase())
+                  )
+                );
+              }
+            }}
             placeholder="Search for the products"
-            loading={loading}
           />
         }
       </div>
@@ -81,7 +86,7 @@ function App() {
       </div>
       <div
         className="flex flex-wrap w-full justify-between overflow-y-auto px-[24px] custom-scroll"
-        style={{ maxHeight: "74vh" }}
+        style={{ maxHeight: "65vh" }}
         ref={containerRef}
       >
         {filteredData?.slice(0, itemsToShow)?.map((data, index) => (
@@ -108,19 +113,68 @@ function App() {
 export default App;
 
 const ProductCard = (props) => {
+  const data = {
+    apple:
+      "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-bluetitanium?wid=5120&hei=2880&fmt=p-jpg&qlt=80&.v=VW44dkRidm5wazhwcGxtL0cyaEJ2VTkrNXBUdUJSK1k4NE5seUtJaW80Y1JhMkZic05CWW9VU1dxSWZUUWVyT3B3azBNWWRMTTJUR1Y1SkFQMTJmYytRU0p3cnVrZzhmdWQwTDVYRlMxUFpLVUtTU1p5Q3hjSjJnWUpQQWJXd2pMZklqcUZDbWdZWTl4MzFJMU9ocFJBPT0=&traceId=1",
+    samsung:
+      "https://www.myg.in/images/thumbnails/624/460/detailed/51/S24ULTRAGREY1.JPEG",
+    vivo: "https://www.cellspare.com/image/cache/catalog/data/Accessories%20Villa/Product%20image/vivo-x80-rear-housing-panel-module-urben-blue-1000x1000.jpg",
+    oppo: "https://m-cdn.phonearena.com/images/phones/62918-940/OPPO-F1s.jpg?w=1",
+  };
+
   return (
     <div className="p-[16px]">
       <h2 className="w-[300px] pb-[16px]">{props.data.productName}</h2>
       <a href={props.data.url} target="_blank" rel="noopener noreferrer">
         <img
-          src="https://via.placeholder.com/300"
+          className="w-[300px] h-[300px]"
+          style={{ objectFit: "cover" }}
+          src={data[detectBrand(props.data.productName)]}
           alt={props.data.productName}
         />
       </a>
       <div className="flex justify-between">
+        <p className="capitalize">{detectShop(props.data.url)}</p>
         <p>Price: ₹{props.data.price}</p>
         <p>Ratings: {props.data.ratings} ★</p>
       </div>
     </div>
   );
+};
+
+export const detectBrand = (productName) => {
+  const brands = [
+    "Apple",
+    "Samsung",
+    "Huawei",
+    "Xiaomi",
+    "OnePlus",
+    "Google",
+    "Sony",
+    "LG",
+    "Oppo",
+    "Vivo",
+    "Nokia",
+    "Motorola",
+  ];
+
+  for (const brand of brands) {
+    if (productName.toLowerCase().includes(brand.toLowerCase())) {
+      return brand.toLocaleLowerCase();
+    }
+  }
+
+  return "Unknown Brand";
+};
+
+export const detectShop = (url) => {
+  const shops = ["Amazon", "Flipkart", "Chroma"];
+
+  for (const shop of shops) {
+    if (url.toLowerCase().includes(shop.toLowerCase())) {
+      return shop.toLocaleLowerCase();
+    }
+  }
+
+  return "Unknown Shop";
 };
